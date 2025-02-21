30363305ee1519b9d536694100c176b4a78c8eef
Lara Fernanda <larafvitoriano@gmail.com>
2025-02-21 00:39:59 -0300

    feat: add shadcn initial setup

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <>
+      <h1 className="text-red-500">Oi</h1>
+      <Button>FSW 7.0</Button>
+    </>
+  );
+};
+
+export default ProductPage;
