19460af8a3f810c4fa3305b48aa51529de6f34bf
Lara Fernanda <larafvitoriano@gmail.com>
2025-02-26 22:34:15 -0300

    feat: add cart context and cart sheet

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -3,11 +3,14 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
+
+import CartSheet from "../../components/cart-sheet";
+import { CartContext } from "../../contexts/cart";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{ 
@@ -21,6 +24,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const {toggleCart} = useContext(CartContext);
   const [quantity, setQuantity] = useState<number>(1);
   const handleDecreaseQuantity = () => {
     setQuantity((prev) => {
@@ -33,8 +37,12 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
   const handleIncreaseQuantity = () => {
     setQuantity((prev) => prev + 1);
   };
+  const handleAddToCart = () => {
+    toggleCart();
+  }
 
   return (
+    <>
     <div className="relative z-50 rounded-t-3xl p-5 mt-[-1.5rem] flex-auto flex flex-col overflow-hidden">
         <div className="flex-auto overflow-hidden">
         {/* RESTAURANTE */}
@@ -85,8 +93,10 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
         </ScrollArea>
         </div>
 
-        <Button className="w-full rounded-full mt-6">Adicionar à sacola</Button>
+        <Button className="w-full rounded-full mt-6" onClick={handleAddToCart}>Adicionar à sacola</Button>
       </div>
+      <CartSheet />
+      </>
   );
 }
 
