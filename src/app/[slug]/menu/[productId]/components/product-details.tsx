e632e79063030c9b9eb9838554fbcafdd657f5a1
Lara Fernanda <larafvitoriano@gmail.com>
2025-02-26 22:06:52 -0300

    feat: finishing product details page

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -6,7 +6,9 @@ import Image from "next/image";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
+
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{ 
     include: { 
@@ -33,16 +35,19 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
   };
 
   return (
-    <div className="relative z-50 rounded-t-3xl p-5 mt-[-1.5rem] flex-auto flex flex-col">
-        <div className="flex-auto">
+    <div className="relative z-50 rounded-t-3xl p-5 mt-[-1.5rem] flex-auto flex flex-col overflow-hidden">
+        <div className="flex-auto overflow-hidden">
+        {/* RESTAURANTE */}
         <div className="flex items-center gap-1.5">
           <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full"/>
           <p className="text-xs text-muted-foreground">{product.restaurant.name}</p>
         </div>
 
+        {/* NOME DO PRODUTO */}
         <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
 
-        <div className="flex items-center justify-between">
+        {/* PREÇO E QUANTIDADE */}
+        <div className="mt-3 flex items-center justify-between">
           <h3 className="text-xl font-semibold">
           {formatCurrency(product.price)}
           </h3>
@@ -57,18 +62,27 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </div>
         </div>
 
+        <ScrollArea className="h-full">
+        {/* SOBRE */}
         <div className="mt-6 space-y-3">
           <h4 className="font-semibold">Sobre</h4>
           <p className="text-sm text-muted-foreground">{product.description}</p>
         </div>
 
+        {/* INGREDIENTS */}
         <div className="mt-6 space-y-3">
           <div className="flex items-center gap-1 5">
             <ChefHatIcon size={18}/>
             <h4 className="font-semibold">Ingredientes</h4>
           </div>
-          <p className="text-sm text-muted-foreground">{product.description}</p>
+          <ul className="text-muted-fo list-disc px-5 text-sm text-muted-foreground">
+                {product.ingredients.map((ingredient) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ul>
+
         </div>
+        </ScrollArea>
         </div>
 
         <Button className="w-full rounded-full mt-6">Adicionar à sacola</Button>
--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -11,9 +11,12 @@ interface ProductPageProps {
 }
 
 const ProductPage = async ({ params }: ProductPageProps) => {
-  const { productId } = await params;
-  const product = await db.product.findUnique({ where: { id: productId }, include: { restaurant: { select: { name: true, avatarImageUrl: true, } } } });
+  const { slug, productId } = await params;
+  const product = await db.product.findUnique({ where: { id: productId }, include: { restaurant: { select: { name: true, avatarImageUrl: true, slug: true} } } });
   if (!product) {
+    return notFound();
+  }
+  if (product.restaurant.slug.toUpperCase != slug.toUpperCase) {
     return notFound();
   }
   return (
