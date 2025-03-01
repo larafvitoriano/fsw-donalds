d43a814300a5153f74b799916e5eb39a2c148126
Lara Fernanda <larafvitoriano@gmail.com>
2025-02-28 21:37:17 -0300

    feat: add button finalizar pedido

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -11,6 +11,8 @@ export interface CartProduct
 export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
+  total: number;
+  totalQuantity: number;
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
@@ -20,6 +22,8 @@ export interface ICartContext {
 
 export const CartContext = createContext<ICartContext> ({
   isOpen: false,
+  total: 0,
+  totalQuantity: 0,
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
@@ -31,6 +35,13 @@ export const CartContext = createContext<ICartContext> ({
 export const CartProvider = ({children}: {children: ReactNode}) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const total = products.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+  }, 0);
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
 
   const toggleCart = () => {
     setIsOpen(prev => !prev);
@@ -91,7 +102,9 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
       addProduct,
       decreaseProductQuantity,
       increaseProductQuantity,
-      removeProduct
+      removeProduct,
+      total,
+      totalQuantity,
     }}
     >
       {children}
